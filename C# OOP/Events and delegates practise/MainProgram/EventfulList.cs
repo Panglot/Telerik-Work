32b236ce704243ca076d57539f20ba495dddc26d
Panglot <DobromirBogatev@gmail.com>
2016-07-09 20:10:04 +0300

    Push This again

---
+++ C# OOP/Events and delegates practise/MainProgram/EventfulList.cs
@@ -0,0 +1,52 @@
+﻿namespace MainProgram
+{
+    using System;
+    using System.Collections.Generic;
+
+    class EventfulList<T>
+    {
+        // The keyword event says its an event. EventHandler is predefined delegate.
+        // It should be public so subscribers can access it.
+        // We can create our own delegate with the same signature.
+        // public delegate void ChangedEventHandler(object sender, EventArgs e);
+        // and then again create instance of our delegate
+        // public event ChangedEventHandler customEventForChange;
+        public event EventHandler customEventForChange; 
+
+        private List<T> data;
+
+        public EventfulList()
+        {
+            this.data = new List<T>();
+        }
+
+        // OnChange is our method that can be named as we want. The convention is On[event]
+        // The method "raises" the event. By raises means that it invokes all the methods
+        // attached to our EventHandle delegate. Every time you call this mehtod it tries
+        // to invoke all the methods attached. It doesnt care who or what is attached.
+        // This is loose coupling. This class has no connection to any other class, while
+        // any other class can attach to its event.
+        private void OnChange()
+        {
+            if (customEventForChange != null)
+            {
+                customEventForChange.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void Add(T value)
+        {
+            data.Add(value);
+            // Here we raise the event. We say "Something happened. Whoever is attached
+            // should know about it. Do your stuff!".
+            this.OnChange(); 
+        }
+
+        public void Clear()
+        {
+            data.Clear();
+            this.OnChange(); // We raise the event again.
+        }
+
+    }
+}
---
+++ C# OOP/Events and delegates practise/MainProgram/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace MainProgram
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventfulList<int> testList = new EventfulList<int>(); // Creating instace of our class.
+
+            // This is where we subscribe for the ven of our class. Since customEventForChange is
+            // public we can access it. As delegate it allows us to add methods to it.
+            // We add our method OnChange. It must have the signature of EventHandler which is
+            // return type void and arguments (object, EventArgs). Since we attached our method
+            // every time the class raises the event our method will be invoked. This way we are
+            // subscribed for the event and can do something every time its raised.
+            testList.customEventForChange += OnChange; 
+
+            testList.Add(5);
+            testList.Add(2);
+            testList.Add(15);
+            testList.Clear();
+
+            Console.WriteLine();
+
+
+            EventfulList<int> testList2 = new EventfulList<int>(); // Second instance just for the example.
+
+            testList2.customEventForChange += OnChange;
+
+            testList2.Add(5);
+            testList2.Add(2);
+            testList2.Add(15);
+            testList2.Clear();
+
+
+        }
+
+        // This is the method we subscribe (attach) to the event. Notice that it has the same signature
+        // as the event delegate: void return type and arguments (object, EventArgs). Every time the event
+        // is raised, this method will be invoked. In this case we just print stuff on the console to show
+        // that its working, but we can do whatever we want. 
+        static void OnChange(object sender, EventArgs args)
+        {
+            Console.WriteLine("Event tracks for list changes. List {0} has been changed!", sender.GetHashCode());
+        }
+    }
+}
