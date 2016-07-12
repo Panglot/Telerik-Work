986af597ca7538df5915d3d9e4e264b1794d69dd
Panglot <DobromirBogatev@gmail.com>
2016-07-13 00:12:52 +0300

    WPF Tutorial Start

---
+++ C# OOP/WPF Tutorial/Youtube Tutorial/01-Intro/VeryBasicApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace VeryBasicApp
+{
+    using System.Windows;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void DemoButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello The button was clicked", "Caption for the box");
+        }
+
+        private void DemoButton2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("The second button was clicked", "Reduce dulness");
+        }
+        private void BaseClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("The base executes", "Aaand another caption");
+        }
+    }
+}
